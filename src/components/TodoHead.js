624a1256ac617a45b1f220b488f3056184179f08
seasonedSedum <pado1144@gmail.com>
2023-01-27 18:41:12 +0900

    일자 수정

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -3,8 +3,8 @@ import classes from "./TodoHead.module.css";
 const TodoHead = () => {
   return (
     <div className={classes.headblock}>
-      <h1>2019년 7월 10일</h1>
-      <div className={classes.day}>수요일</div>
+      <h1>2023년 1월 27일</h1>
+      <div className={classes.day}>금요일</div>
       <div className={classes["tasks-left"]}>할 일 2개 남음</div>
     </div>
   );
