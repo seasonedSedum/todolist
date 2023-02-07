751fc21a6c0e3cb602de33fd6ef8a540fd0b0e79
seasonedSedum <pado1144@gmail.com>
2023-02-07 21:11:45 +0900

    list를 context에서 가져오게 변경

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,20 @@
 import TodoItem from "./TodoItem";
 import classes from "./TodoList.module.css";
+import { useTodoState } from "../TodoContext";
 
 const TodoList = () => {
+  const todos = useTodoState();
+
   return (
     <div className={classes.listBlock}>
-      <TodoItem text="프로젝트 생성하기" done={true} />
-      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-      <TodoItem text="Context 만들기" done={false} />
-      <TodoItem text="기능 구현하기" done={false} />
+      {todos.map((todo) => (
+        <TodoItem
+          id={todo.id}
+          key={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </div>
   );
 };
